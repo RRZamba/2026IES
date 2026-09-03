94d24048e5a4a0746a043385864e53585fe031a8
RRZamba <114082625+RRZamba@users.noreply.github.com>
2026-09-03 11:25:57 -0300

    Add files via upload

---
+++ 06_Aula_TécnicasLevantamentoDeRequisitos/Sistema_Emprestimo/Login/_login.php
@@ -0,0 +1,8 @@
+<?php
+    //1) Recuperando os valores
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+    
+    //2) Mostrar os valores
+    echo "Olá $login !!! Sua senha é: $senha ! Te amo!";
+?>
